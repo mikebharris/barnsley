25a60a12c5ae85fe5dbe4a1b1197c105644d5a46
naomi <naomi@happycatsin.space>
2022-10-05 15:56:44 +0100

    different colours for different functions

--- barnsley.go
+++ barnsley.go
@@ -11,29 +11,38 @@ import (
 func main() {
 	width := 1024
 	height := 1256
-	var iters = 10000000
-	png.Encode(os.Stdout, generateBarnsleyFern(width, height, iters))
+	var iterations = 10000000
+
+	png.Encode(os.Stdout, generateBarnsleyFern(width, height, iterations))
 }
 
-func generateBarnsleyFern(width int, height int, iters int) *image.RGBA {
+func generateBarnsleyFern(width int, height int, iterations int) *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	xratio, yratio := calculateCanvasRatios(width, height)
+	xRatio, yRatio := calculateCanvasRatios(width, height)
 	fernGreen := color.RGBA{R: 000, G: 128, B: 000, A: 0xff}
+	fernRed := color.RGBA{R: 128, G: 000, B: 000, A: 0xff}
+	fernBlue := color.RGBA{R: 000, G: 000, B: 128, A: 0xff}
+	fernYellow := color.RGBA{R: 128, G: 128, B: 000, A: 0xff}
 
 	var x float64 = 0
 	var y float64 = 0
-	for i := 0; i < iters; i++ {
+	for i := 0; i < iterations; i++ {
 		r := rand.Intn(100)
+		var colour color.RGBA
 		if r == 1 {
 			x, y = generateStem(x, y)
+			colour = fernGreen
 		} else if r < 87 {
 			x, y = generateSuccessivelySmallerLeaflets(x, y)
+			colour = fernBlue
 		} else if r < 94 {
 			x, y = generateLargestLeftHandLeaflet(x, y)
+			colour = fernRed
 		} else {
 			x, y = generateLargestRightHandLeaflet(x, y)
+			colour = fernYellow
 		}
-		img.Set(int(x*xratio)+width/2, int(y*yratio), fernGreen)
+		img.Set(int(x*xRatio)+width/2, int(y*yRatio), colour)
 	}
 	return img
 }
